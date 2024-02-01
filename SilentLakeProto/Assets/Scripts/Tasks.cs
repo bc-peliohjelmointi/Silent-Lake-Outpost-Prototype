93e82418ce732aab4b0dc7e9ac0731543bf4d81d
LeeviSuominen <115700342+LeeviSuominen@users.noreply.github.com>
2024-02-01 14:53:50 +0200

    Started working on tasklist

---
+++ SilentLakeProto/Assets/Scripts/Tasks.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Tasks : MonoBehaviour
+{
+    [SerializeField] GameObject taskBarrier;
+    [SerializeField] TMP_Text taskText;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpotAnimal()
+    {
+
+    }
+}
