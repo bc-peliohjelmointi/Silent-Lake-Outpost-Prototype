203f8d86c97b048c00c5c4cf35d64088b8e44764
RuikkaAnton <115700078+RuikkaAnton@users.noreply.github.com>
2024-02-05 10:23:18 +0200

    Zombie script changes

--- SilentLakeProto/Assets/Scripts/ZombieAINav.cs
+++ SilentLakeProto/Assets/Scripts/ZombieAINav.cs
@@ -16,73 +16,81 @@ public class ZombieAINav : MonoBehaviour
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-
-        if (!agent)
+        if (!Chase.activeSelf)
         {
-            Debug.LogError("NavMeshAgent component not found.");
-        }
-        else
-        {
-            SetRandomDestination();
+            agent = GetComponent<NavMeshAgent>();
 
-            void OnTriggerEnter(Collider other)
+            if (!agent)
             {
-                if (other.CompareTag("Player"))
+                Debug.LogError("NavMeshAgent component not found.");
+            }
+            else
+            {
+                SetRandomDestination();
+
+
+
+                void Update()
                 {
 
-                    agent.SetDestination(Player.position);
-
-                    Chase.SetActive(true);
-
-                }
-            }
-
-            void Update()
-            {
-
-                if (agent.remainingDistance < 0.5f)
-                {
-                    SetRandomDestination();
-                }
-
-
-            }
-
-            void SetRandomDestination()
-            {
-                Vector3 randomPoint = GetRandomPointInNavMesh();
-                agent.SetDestination(randomPoint);
-            }
-
-            Vector3 GetRandomPointInNavMesh()
-            {
-                Vector3 randomPoint = Vector3.zero;
-
-                NavMeshHit hit;
-                for (int i = 0; i < 30; i++)
-                {
-                    float randomX = Random.Range(-500f, 500f);
-                    float randomZ = Random.Range(-500f, 500f);
-
-                    randomPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-                    if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+                    if (agent.remainingDistance < 0.5f)
                     {
-                        return hit.position;
+                        SetRandomDestination();
                     }
+
+
                 }
 
-
-                return randomPoint;
-
-                if (Chase.activeSelf)
+                void SetRandomDestination()
                 {
-                    agent.SetDestination(Player.position);
+                    Vector3 randomPoint = GetRandomPointInNavMesh();
+                    agent.SetDestination(randomPoint);
                 }
 
+                Vector3 GetRandomPointInNavMesh()
+                {
+                    Vector3 randomPoint = Vector3.zero;
+
+                    NavMeshHit hit;
+                    for (int i = 0; i < 30; i++)
+                    {
+                        float randomX = Random.Range(-500f, 500f);
+                        float randomZ = Random.Range(-500f, 500f);
+
+                        randomPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+                        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+                        {
+                            return hit.position;
+                        }
+                    }
+
+
+                    return randomPoint;
+
+
+
+                }
             }
         }
+        
 
+    }
+
+    private void Update()
+    {
+        if (Chase.activeSelf)
+        {
+            agent.SetDestination(Player.position);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            Chase.SetActive(true);
+        }
+        
     }
 }
